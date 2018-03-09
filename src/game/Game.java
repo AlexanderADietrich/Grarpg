f76d382e535e089c61944c7b1af25e3110733bc0
voice <voice@10.20.18.246>
2018-03-09 09:54:40 -0800

    Actually added a listener, it only works when enter is pressed

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,6 +1,8 @@
 package game;
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.net.URL;
@@ -16,21 +18,12 @@ public class Game extends Applet implements Runnable{
     private URL url;
     private Map m = new Map(8, 8, 384, 384);
     private TextField t = new TextField("", 20);
-    private KeyListener k = new KeyListener() {
+    private ActionListener k = new ActionListener() {
         @Override
-        public void keyTyped(KeyEvent e) {
-            
+        public void actionPerformed(ActionEvent e) {
+            System.out.println(e.getActionCommand());
         }
-
-        @Override
-        public void keyPressed(KeyEvent e) {
-            e.getKeyChar();
-        }
-
-        @Override
-        public void keyReleased(KeyEvent e) {
-            
-        }
+        
     };
     
     /*
@@ -40,6 +33,7 @@ public class Game extends Applet implements Runnable{
         setSize(584, 384);
         setLayout(null);
         
+        t.addActionListener(k);
         add(t);
         
         
