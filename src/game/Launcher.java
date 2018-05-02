3b1d4974e7953165a01403ecc0f9dbbb81083682
XxXx__REE__xXxX <voiceless1848@gmail.com>
2018-05-02 09:50:26 -0700

    Code courtesy of Roedy Green allows applets to effectively be turned into applications. However, this also means all images must be loaded outside the applet as it will only load images when ran directly.

---
+++ src/game/Launcher.java
@@ -0,0 +1,9 @@
+package game;
+public class Launcher {
+    public static void main(String[] args){
+        //Fire Up applet using Hybrid.java
+        Game g = new Game();
+        Hybrid.fireup(g, "REEE", 584, 384);
+        //Load All Images and pass them to Applet
+    }
+}
